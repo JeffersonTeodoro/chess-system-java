06f15445c486fb51904b8c1570354622b7e33054
jefferson F Teodoro <jeffersonfranca199@gmail.com>
2025-10-11 06:44:42 -0300

    Starting to implement Board and Piece

--- src/main/java/com/AltaTech/application/Program.java
+++ src/main/java/com/AltaTech/application/Program.java
@@ -1,13 +1,11 @@
 package com.AltaTech.application;
 
-import com.AltaTech.boardgame.Position;
+import com.AltaTech.boardgame.Board;
 
 public class Program {
     public static void main(String[] args){
 
-        Position pos = new Position(3,5);
-                System.out.println(pos);
-
+        Board board = new Board(8,8);
 
     }
 }
