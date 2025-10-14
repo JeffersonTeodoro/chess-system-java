66ce6d5fa16364d1aefbb1c5e96fadf5e55783f7
jefferson F Teodoro <jeffersonfranca199@gmail.com>
2025-10-14 06:16:47 -0300

    Check logic

--- src/main/java/com/AltaTech/application/Program.java
+++ src/main/java/com/AltaTech/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
         List<ChessPiece> captured = new ArrayList<>();
 
         while (true) {
-            try {
+           try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
                 System.out.println();
@@ -36,7 +36,7 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-            }
+           }
             catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
